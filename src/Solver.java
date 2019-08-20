1a9d4bdc6156e2a301f86aa96eaecadc13828269
iryna.sribna <iryna.sribna@awin.com>
2019-08-20 17:35:51 +0200

    Start Solver

--- src/Solver.java
+++ src/Solver.java
@@ -4,11 +4,15 @@ import edu.princeton.cs.algs4.Queue;
 
 public class Solver {
 
-    private MinPQ<Board> pQueue;
+    private MinPQ<SearchNode> pQueue;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
-        this.pQueue.insert(initial);
+
+        SearchNode initialSearchNode = new SearchNode(initial, 0, null);
+        this.pQueue.insert(initialSearchNode);
+
+
     }
 
     // is the initial board solvable? (see below)
@@ -29,8 +33,15 @@ public class Solver {
     private static class SearchNode
         implements Comparable<SearchNode> {
 
-        private int priority;
+        private Board board;
+        private int moves;
         private SearchNode previous;
+
+        public SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+        }
 
         @Override
         public int compareTo(SearchNode o) {
